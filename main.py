12b5fbbed3b9b0a78ed9370e2f03e368c50d3a98
SeleniumTraining <momin.khanzada1999@gmail.com>
2024-09-06 23:35:46 +0500

    logged in into saucedemo site succefully

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from selenium import webdriver
 import os
 
+from selenium.webdriver.common.by import By
+
 # os.environ['PATH'] += r"C:\Program Files (x86)\edgedriver_win64\msedgedriver.exe"
 
 driver = webdriver.Firefox()
 
-driver.get("https://www.selenium.dev/")
+username = "standard_user"
+userpassword  = "secret_sauce"
+url = "https://www.saucedemo.com/"
 
-driver.maximize_window()
+driver.get(url)
 
-title = driver.title
+username_field = driver.find_element(By.ID, value='user-name')
+password_field = driver.find_element(By.ID, value='password')
 
-print(title)
+login_button = driver.find_element(By.ID, value='login-button')
 
-# assert "Seleniuewewm" in title
+assert not login_button.get_attribute("disabled")
 
+username_field.send_keys(username)
+password_field.send_keys(userpassword)
+
+login_button.click()
+
+
+success_element = driver.find_element(By.CSS_SELECTOR, value='.title')
+assert success_element.text == 'Products'
